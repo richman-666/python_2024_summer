645ecf94327f85e2d7b7a21d098f56ad1c470a87
richman-666 <ericchen1011017@gmail.com>
2024-08-06 20:03:42 +0800

    Update ex4-2.py

--- ex4-2.py
+++ ex4-2.py
@@ -1,3 +1,10 @@
 a = "Hello, World!"
 print(a[0])
 #you will get "H"
+txt = "The best things in life are free!"
+if "free" in txt:
+  print("Yes, 'free' is present.")
+  #you will get "Yes, 'free' is present."
+  b = "Hello, World!"
+print(b[:5])
+#you will get "Hello"
