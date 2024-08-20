aea2843528df7aeda5ac62513177db6299b7488b
richman-666 <ericchen1011017@gmail.com>
2024-08-20 19:20:04 +0800

    Create ex6-1.py

---
+++ ex6-1.py
@@ -0,0 +1,31 @@
+for x in "why i need line text????????":
+  print(x)
+  #you will get 
+  w
+  h
+  y
+
+  i
+  
+  n
+  e
+  e
+  d
+
+  l
+  i
+  n
+  e
+
+  t
+  e
+  x
+  t
+  ?
+  ?
+  ?
+  ?
+  ?
+  ?
+  ?
+  ?
